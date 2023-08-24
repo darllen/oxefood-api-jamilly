d2b1aa57a37dc6be9ee8b030b4e7b3e9e2c98531
Aluno <aluno@ifpe>
2023-08-24 11:01:59 -0300

    criando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiJamillyApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiJamillyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiJamillyApplication.class, args);
+	}
+
+}
